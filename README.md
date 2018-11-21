a3e29e2ff9c410b3cb5521004733705607b712ba
tslp <38236653+tslp@users.noreply.github.com>
2018-11-21 13:16:29 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# ppsuvt
+ 一键部署到 heroku  [![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/osaacpik/rayv43l)
+
+
+
+
+
---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+cd /v2raybin
+
+#wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
+
+cat xaa xab xac > v2ray.zip
+unzip v2ray.zip 
+
+cd /v2raybin/v2ray*
+
+
+chmod +x v2ray
+chmod +x v2ctl
+
+echo -e -n "$CONFIG_JSON1" > config.json
+echo -e -n "$PORT" >> config.json
+echo -e -n "$CONFIG_JSON2" >> config.json
+echo -e -n "$UUID" >> config.json
+echo -e -n "$CONFIG_JSON3" >> config.json
+
+./v2ray
